9d469a075f0d34a4c58808f17456891bc74cf86a
keccakdog <keccakdog@gmail.com>
2024-08-16 00:11:35 -0700

    update deployscrpt

--- script/DeployBadge2.sol
+++ script/DeployBadge2.sol
@@ -18,42 +18,42 @@ contract DeployBadge2 is Script {
         NuriBadge100 nuriBadge100 = new NuriBadge100(
             0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113,
             0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F,
-            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/images/nuri-locker-100.png"
+            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/badge100.json"
         );
         console.log(address(nuriBadge100));
 
         NuriBadge200 nuriBadge200 = new NuriBadge200(
             0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113,
             0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F,
-            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/images/nuri-locker-200.png"
+            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/badge200.json"
         );
         console.log(address(nuriBadge200));
 
         NuriBadge500 nuriBadge500 = new NuriBadge500(
             0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113,
             0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F,
-            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/images/nuri-locker-500.png"
+            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/badge500.json"
         );
         console.log(address(nuriBadge500));
 
         NuriBadge1000 nuriBadge1000 = new NuriBadge1000(
             0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113,
             0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F,
-            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/images/nuri-locker-1000.png"
+            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/badge1000.json"
         );
         console.log(address(nuriBadge1000));
 
         NuriBadge5000 nuriBadge5000 = new NuriBadge5000(
             0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113,
             0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F,
-            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/images/nuri-locker-5000.png"
+            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/badge5000.json"
         );
         console.log(address(nuriBadge5000));
 
         NuriBadge10000 nuriBadge10000 = new NuriBadge10000(
             0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113,
             0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F,
-            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/images/nuri-locker-10000.png"
+            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/badge10000.json"
         );
         console.log(address(nuriBadge10000));
         vm.stopBroadcast();
