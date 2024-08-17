2f3c2851afd6330d0ca12cf7c127cc4abf6b0a80
keccakdog <keccakdog@gmail.com>
2024-08-16 23:58:26 -0700

    update deploy scripts

---
+++ script/DeployLPBadge.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {NuriNativeLP} from "../src/badge/NuriNativeLP.sol";
+
+contract DeployLPBadge is Script {
+    uint256 DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+    function run() external {
+        vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
+        NuriNativeLP nurinativelp = new NuriNativeLP(
+            0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113,
+            "https://raw.githubusercontent.com/RamsesExchange/nuri-canvas-contracts/master/badge100.json"
+        );
+        console.log(address(nurinativelp));
+
+        vm.stopBroadcast();
+    }
+}
